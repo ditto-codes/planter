d6269fdb96e2fa2f32f7b1f70b26e55a5e7a9896
Matthew Alicea <matthew.ivan.alicea@gmail.com>
2021-06-12 19:31:09 -0400

    added simple script
    Wrote a simple script that creates a .planter dir in home dir. This is
    mostly for testing, as I wanted to make sure scripts run by npx could
    interact with the user's file system. Later on, if we want to let users
    create aliases, those can be saved in something like
    `<home-dir>/.planter/alias.json`, and read during the cli execution.

---
+++ index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+const homedir = require('os').homedir();
+const path = require('path');
+const fs = require('fs');
+
+let dir = path.join(homedir, '.planter');
+
+fs.mkdirSync(dir);
