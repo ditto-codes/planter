662e395c6097b85107449c67cb51d123ba5849b1
Matthew Alicea <malicea@petermillar.com>
2022-07-01 18:33:59 -0400

    set up the project structure a bit; started using commander

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
-const homedir = require('os').homedir();
-const path = require('path');
-const fs = require('fs');
+// const homedir = require('os').homedir();
+// const path = require('path');
+// const fs = require('fs');
 
-let dir = path.join(homedir, '.planter');
+// let dir = path.join(homedir, '.planter');
 
-fs.mkdirSync(dir);
+// fs.mkdirSync(dir);
+
+import cli from './lib/cli.js';
+cli();
