411d70eaf7cad29310a2c401e17c2225d1e17118
Matthew Alicea <matthew.ivan.alicea@gmail.com>
2022-12-07 20:07:52 -0500

    fix: removed default planters; fixes #22

--- scripts/config_default.js
+++ scripts/config_default.js
@@ -5,18 +5,7 @@ import fs from 'fs-extra';
 import { planterSchema } from "../lib/utils.js";
 
 export const defaultConfig = {
-  planters: { 
-    ...planterSchema('basic', {
-      location: 'https://github.com/matthew-ia/planter-basic',
-      position: 1,
-      added: '2022-07-12-20:15:58-04',
-    }), 
-    ...planterSchema('svelte', { 
-      location: 'https://github.com/matthew-ia/planter-svelte',
-      position: 2,
-      added: '2022-07-12-20:15:59-04',
-    }),
-  }, 
+  planters: { }, 
 }
 
 fs.outputJSONSync(path.resolve('./.planter/config.json'), defaultConfig, { spaces: 2 });
--- scripts/config_test.js
+++ scripts/config_test.js
@@ -3,11 +3,20 @@
 import path from 'path';
 import fs from 'fs-extra';
 import { planterSchema } from "../lib/utils.js";
-import { defaultConfig } from './config_default.js';
+import { internalConfig } from './config_internal.js';
 
 const config = {
-  planters: { 
-    ...defaultConfig.planters,
+  planters: {
+    ...planterSchema('basic', {
+      location: 'https://github.com/matthew-ia/planter-basic',
+      position: 1,
+      added: '2022-07-12-20:15:58-04',
+    }), 
+    ...planterSchema('svelte', { 
+      location: 'https://github.com/matthew-ia/planter-svelte',
+      position: 2,
+      added: '2022-07-12-20:15:59-04',
+    }),
     ...planterSchema('howdy', {
       location: 'repo-howdy',
     }),
